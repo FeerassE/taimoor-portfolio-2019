d90e34c8262a14ff4ff0c5d19c1b32219fddefba
Feerass Ellid <feerasse@gmail.com>
2019-05-09 09:51:11 -0400

    fixed images

--- gallery-script.js
+++ gallery-script.js
@@ -32,13 +32,13 @@ for(let i = 1; i <= 115; i++){
 $(".gallery-image").click(function(event){
     let source = $(this).find('img').attr('src')
 
-    $(".main").append(`<div class="modal"></img><img class="modal-img" src=${source}><img class="close" src=./icons/white_x.png></img></div>`);
+    $(".main").append(`<div class="modal"></img><img class="modal-img" src=${source}></img></div>`);
     $("body").addClass("modal-open");
 
 })
 
 
-$("body").on("click",".close",function(event){
+$("body").on("click",".modal",function(event){
     $(".modal").remove();
     $("body").removeClass("modal-open");
 })
